5f07c1d5f8b7937050c053decf3dd7e062e00488
Alberto Rodríguez Granados <35809433+AlbertoRodr@users.noreply.github.com>
2019-01-16 17:55:59 +0100

    Create Calculos.ts

---
+++ Calculos.ts
@@ -0,0 +1,18 @@
+class Calculos{
+	constructor(){
+	}
+
+	public sumar(numeroUno: number, numeroDos: number):number{
+		return numeroUno+numeroDos;
+	}
+	public restar(numeroUno: number, numeroDos: number):number{
+		return numeroUno-numeroDos;
+	}
+	public multiplicar(numeroUno: number, numeroDos: number):number{
+		return numeroUno*numeroDos;
+	}
+	public dividir(numeroUno: number, numeroDos: number):number{
+		return numeroUno/numeroDos;
+	}
+
+}
